e6441e3f6e3082a57304e778892a436b6f439fe6
Arittro7 <arittro007@gmail.com>
2024-06-19 00:37:18 +0600

    Filter function applied

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,31 +1,54 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import {getStoredJobApplication} from "../../Utility/localStorage"
+import { getStoredJobApplication } from "../../Utility/localStorage";
 import AppliedJobsCard from "./AppliedJobsCard";
 
 const AppliedJobs = () => {
+  const jobs = useLoaderData();
 
-    const jobs = useLoaderData();
+  const [jobsApplied, setJobApplied] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
 
-    const [jobsApplied, setJobApplied] = useState([]);
+  const handleFilterJobs = filter => {
+    if (filter === 'all'){
+      setDisplayJobs(jobsApplied);
+    }
+    else if (filter === 'remote'){
+      const remoteJobs = jobsApplied.filter(job => job.remote_or_onsite === "Remote");
+      setDisplayJobs(remoteJobs);
+    }
+    else if (filter === 'onsite'){
+      const onsiteJobs = jobsApplied.filter(job => job.remote_or_onsite === "Onsite");
+      setDisplayJobs(onsiteJobs);
+    }
+  }
 
-    useEffect(() =>{
-      const storedJobsIds = getStoredJobApplication();
-      if (jobs.length > 0){
-        const jobsApplied = jobs.filter(job => storedJobsIds.includes(job.id))
-        // console.log(jobsApplied);
-        setJobApplied(jobsApplied)
-      }
-    },[])
+  useEffect(() => {
+    const storedJobsIds = getStoredJobApplication();
+    if (jobs.length > 0) {
+      const jobsApplied = jobs.filter((job) => storedJobsIds.includes(job.id));
+      // console.log(jobsApplied);
+      setJobApplied(jobsApplied);
+    }
+  }, []);
 
   return (
     <div>
-      <h2 className="text-3xl font-semibold">Total Applied Jobs : {jobsApplied.length}  </h2>
-
+      <h2 className="text-3xl font-semibold">
+        Total Applied Jobs : {jobsApplied.length}{" "}
+      </h2>
+      <details className="dropdown">
+        <summary className="m-1 btn">Filter</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li onClick={() => handleFilterJobs ('all')}><a>All</a></li>
+          <li onClick={() => handleFilterJobs ('remote')}><a>Remote</a></li>
+          <li onClick={() => handleFilterJobs ('onsite')}><a>Onsite</a></li>
+        </ul>
+      </details>
       <div className="mt-36">
-        {
-          jobsApplied.map(job => <AppliedJobsCard key={job.id} job={job}></AppliedJobsCard>)
-        }
+        {displayJobs.map((job) => (
+          <AppliedJobsCard key={job.id} job={job}></AppliedJobsCard>
+        ))}
       </div>
     </div>
   );
