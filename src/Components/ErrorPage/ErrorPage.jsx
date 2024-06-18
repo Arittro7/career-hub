a667ec33e217943df81661fc7243102ddd511e57
Arittro7 <arittro007@gmail.com>
2024-06-19 01:28:53 +0600

    404 page customized

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,19 @@
 import { NavLink } from "react-router-dom";
+import notFound from '../../assets/images/404.jpg'
+import { HiHome } from "react-icons/hi2";
 
 const ErrorPage = () => {
   return (
     <div>
-      <h2>Opps!</h2>
-      <NavLink to={'/'}><button>Home</button></NavLink>
+      <div className="text-center mt-2 flex justify-center ">
+      <NavLink to={'/'}>
+       <button className="text-4xl p-3 font-bold border-2 rounded-xl border-green-500 flex ">  <HiHome className="mr-3 text-green-500"></HiHome>Home</button>
+      </NavLink></div>
+      <div className="flex justify-center">
+      <img className="h-[500px]" src={notFound} alt="" />
+      </div>
     </div>
+
   );
 };
 
